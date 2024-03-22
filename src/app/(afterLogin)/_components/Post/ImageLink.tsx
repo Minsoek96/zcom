dfb7dc38482a269dd73300189720597f0210d7f2
Minsoek96 <FE.minseok96@gmail.com>
2024-03-22 19:57:08 +0900

    refact : MyImage => ImageLink 변경

---
+++ src/app/(afterLogin)/_components/Post/ImageLink.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type ImageType = {
+  id: string;
+  src: string;
+  width: number;
+  height: number;
+};
+const ImageLink = ({ src, id, width, height }: ImageType) => {
+  if (!src) {
+    return null;
+  }
+
+  return (
+    <Link href={id}>
+      <Image
+        src={src}
+        alt={id}
+        width={width}
+        height={height}
+        style={{ borderRadius: "9999px" }}
+      />
+    </Link>
+  );
+};
+
+export default ImageLink;
