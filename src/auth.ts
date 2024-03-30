291f373bef17b98222de8629f9cfef05cee8245b
Minsoek96 <FE.minseok96@gmail.com>
2024-03-30 15:33:22 +0900

    feat : next-auth 적용

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,54 @@
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
 } = NextAuth({
+  pages: {
+    signIn: '/i/flow/login',
+    newUser: '/i/flow/signup',
+  },
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/login`,
+          {
+            method: 'POST',
+            body: JSON.stringify({
+              id: credentials.username,
+              password: credentials.password,
+            }),
+            headers: { 'Content-Type': 'application/json' },
+          },
+        );
+        const user = await res.json();
 
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
 });
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,10 @@
 import { HttpResponse, http } from 'msw';
 
+const User = [
+  { id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg' },
+  { id: 'zerohch0', nickname: '제로초', image: '/5Udwvqim.jpg' },
+];
+
 const handlers = [
   http.get('/api/trends', () => HttpResponse.json([
     { tagId: 1, title: '제로초', count: 1264 },
@@ -12,6 +17,12 @@ const handlers = [
     { tagId: 8, title: '세븐초', count: 1264 },
     { tagId: 9, title: '나인초', count: 1264 },
   ])),
+
+  http.post('/api/login', () => HttpResponse.json(User[1], {
+    headers: {
+      'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+    },
+  })),
 
   http.post(
     '/api/users',
