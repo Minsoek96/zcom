824c2a4cdaae9d0071d35390d832f12c4635e052
Minsoek96 <FE.minseok96@gmail.com>
2024-04-02 02:15:16 +0900

    feat : tanstack/react-query 셋팅

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,20 @@
-import { HttpResponse, http } from 'msw';
+import { HttpResponse, delay, http } from 'msw';
+
+import { faker } from '@faker-js/faker';
+
+function generateDate() {
+  const lastWeek = new Date(Date.now());
+  lastWeek.setDate(lastWeek.getDate() - 7);
+  return faker.date.between({
+    from: lastWeek,
+    to: Date.now(),
+  });
+}
 
 const User = [
   { id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg' },
   { id: 'zerohch0', nickname: '제로초', image: '/5Udwvqim.jpg' },
+  { id: 'coco', nickname: '코코', image: faker.image.avatar() },
 ];
 
 const handlers = [
@@ -45,6 +57,61 @@ const handlers = [
     //   status: 403,
     // });
   ),
+
+  http.get('/api/postRecommends', async () => {
+    await delay(3000);
+    return HttpResponse.json(
+      [
+        {
+          postId: 1,
+          User: User[0],
+          content: `${1} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 2,
+          User: User[0],
+          content: `${2} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 3,
+          User: User[0],
+          content: `${3} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 4,
+          User: User[0],
+          content: `${4} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+            { imageId: 3, link: faker.image.urlLoremFlickr() },
+            { imageId: 4, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+        {
+          postId: 5,
+          User: User[0],
+          content: `${5} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+            { imageId: 3, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+      ],
+    );
+  }),
 ];
 
 export default handlers;
