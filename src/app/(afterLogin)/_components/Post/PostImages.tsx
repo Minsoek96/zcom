89a43b7912b3d31d30bd3470a73fa2c6a438332d
Minsoek96 <FE.minseok96@gmail.com>
2024-04-05 18:47:22 +0900

    feat : PostImages추가 ( 포스트 이미지갯수별 스타일 설정 )

---
+++ src/app/(afterLogin)/_components/Post/PostImages.tsx
@@ -0,0 +1,151 @@
+import { Post } from '@/app/_types/Post';
+
+import Image from 'next/image';
+
+import styled, { css } from 'styled-components';
+
+const OneImage = css`
+  display: flex;
+  > div {
+    width: 100%;
+    height: 100%;
+
+    img {
+      border-radius: 18px;
+    }
+  }
+`;
+
+const TwoImage = css`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+
+  > div {
+    width: 260px;
+    height: 270px;
+  }
+
+  > div:nth-child(1) {
+    img {
+      border-top-left-radius: 18px;
+      border-bottom-left-radius: 18px;
+    }
+  }
+
+  > div:nth-child(2) {
+    img {
+      border-top-right-radius: 18px;
+      border-bottom-right-radius: 18px;
+    }
+  }
+`;
+
+const ThreeImage = css`
+  display: grid;
+  grid-template-areas:
+    "a a b b"
+    "a a c c";
+  gap: 4px;
+
+  > div:nth-child(1) {
+    grid-area: a;
+    img {
+      border-top-left-radius: 18px;
+      border-bottom-left-radius: 18px;
+    }
+  }
+
+  > div:nth-child(2) {
+    grid-area: b;
+    img {
+      border-top-right-radius: 18px;
+    }
+  }
+
+  > div:nth-child(3) {
+    grid-area: c;
+    img {
+      border-bottom-right-radius: 18px;
+    }
+  }
+`;
+
+const FourImage = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 4px;
+
+  > div:nth-child(1) {
+    img {
+      border-top-left-radius: 18px;
+    }
+  }
+
+  > div:nth-child(2) {
+    img {
+      border-top-right-radius: 18px;
+    }
+  }
+
+  > div:nth-child(3) {
+    img {
+      border-bottom-left-radius: 18px;
+    }
+  }
+
+  > div:nth-child(4) {
+    img {
+      border-bottom-right-radius: 18px;
+    }
+  }
+`;
+
+const Container = styled.div<{ count: number }>`
+  width: 505px;
+  height: 270px;
+  margin-top: 11px;
+
+  img:hover {
+    cursor: pointer;
+  }
+
+  ${({ count }) => count === 1 && OneImage}
+
+  ${({ count }) => count === 2 && TwoImage}
+
+  ${({ count }) => count === 3 && ThreeImage}
+
+  ${({ count }) => count === 4 && FourImage}
+`;
+
+const StyledImageWrapper = styled.div`
+  position: relative;
+`;
+
+type PostImagesProps = {
+  post: Post;
+};
+
+export default function PostImages({ post }: PostImagesProps) {
+  const { Images, User } = post;
+
+  if (!Images || Images.length === 0) {
+    return null;
+  }
+
+  return (
+    <Container count={Images.length}>
+      {Images.map((image) => (
+        <StyledImageWrapper key={image.imageId}>
+          <Image
+            src={image.link}
+            alt={`${User.id}'s image`}
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        </StyledImageWrapper>
+      ))}
+    </Container>
+  );
+}
---
+++ src/app/(afterLogin)/_components/Post/PostItem.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import { styled } from 'styled-components';
+
+import Link from 'next/link';
+
+import formatTimeFromNow from '@/app/_utils/day';
+
+import { Post } from '@/app/_types/Post';
+
+import ImageLink from './ImageLink';
+import UserActionButtons from './UserActionButtons';
+import PostImages from './PostImages';
+
+type PostItemProps = {
+  post: Post;
+};
+
+function PostItem({ post }: PostItemProps) {
+  const { id, nickname } = post.User;
+  return (
+    <Container>
+      <PostWrapper>
+        <div>
+          <ImageLink src={post.User.image} id="img" width={40} height={40} />
+        </div>
+        <div>
+          <UserInfoContainer>
+            <Link href={`${id}`}>
+              <span>{nickname}</span>
+            </Link>
+            {' '}
+            <span>
+              @
+              {post.User.id}
+            </span>
+            {' · '}
+            <span>{formatTimeFromNow({ createdAt: post.createdAt })}</span>
+          </UserInfoContainer>
+          <div>
+            {post.Images && <PostImages post={post} />}
+            {post.content}
+          </div>
+          <UserActionButtons />
+        </div>
+      </PostWrapper>
+    </Container>
+  );
+}
+
+export default PostItem;
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+
+  //TODO : 아이템 추가시 스타일조정
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.03);
+    transition: background-color 0.2s ease;
+  }
+`;
+
+const PostWrapper = styled.div`
+  display: flex;
+  padding-inline: 15px;
+  padding-block: 11px;
+  border-bottom: 1px solid #eff3f4;
+  width: 100%;
+
+  > div:first-child {
+    width: 40px;
+  }
+
+  > div:last-child {
+    width: 100%;
+    > div {
+      padding-inline: 12px;
+    }
+
+    > div:last-child {
+      padding-inline: 0;
+    }
+  }
+`;
+
+const UserInfoContainer = styled.div`
+  font-size: 14px;
+  margin-bottom: 2px;
+  > a > span:first-child {
+    font-weight: 700;
+  }
+
+  > span:first-child ~ span {
+    color: #536472;
+  }
+`;
