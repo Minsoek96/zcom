e7651f6347ed8daa1cf049e76a4d4d660be582e7
Minsoek96 <FE.minseok96@gmail.com>
2024-06-11 18:05:29 +0900

    fix : mjs- swc처리 제거

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   images: {
     domains: ['loremflickr.com', 'avatars.githubusercontent.com', 'cloudflare-ipfs.com'],
   },
+  swcMinify: true,
   compiler: {
-    // Styled Components의 SSR을 위한 SWC 플러그인 활성화
     styledComponents: true,
   },
 };
--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: true, // SWC를 사용한 미니파이 활성화 (기본적으로 활성화됨)
-};
-
-export default nextConfig;
